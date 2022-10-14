99ffbe5b0e148688ec56185e0b5fc18e3ea881f5
Daddyaezzy <106809355+Daddyaezzy@users.noreply.github.com>
2022-10-15 00:37:15 +0100

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+//import { getDatabase } from "firebase/database";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
