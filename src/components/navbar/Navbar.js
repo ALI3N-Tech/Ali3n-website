2d6c6a99b801f321ea8cb2a7603252b6bc6be884
Fajobi Anselm <pulumbu11@gmail.com>
2022-10-15 08:51:50 +0100

    date

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import pic from "../../images/ALI3N_LOGO2-1 1.png";
 import { FaBars, FaTimes } from "react-icons/fa";
